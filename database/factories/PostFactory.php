ee2375259ed38f0509605518dd3ffa029fa1210f
menaemg <abdalmonaem.gamal@gmail.com>
2020-05-15 11:51:37 +0200

    Updtate factory to srore image

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -5,14 +5,19 @@
 use App\Post;
 use App\User;
 use App\Category;
+use Illuminate\Support\Facades\Storage;
 use Faker\Generator as Faker;
 
 $factory->define(Post::class, function (Faker $faker) {
+    $url = "https://picsum.photos/id/" . rand(1,50) . "/400/300.jpg";
+    $contents = file_get_contents($url);
+    $imageName = $faker->md5() . '.jpg';
+    Storage::disk('images')->put( $imageName , file_get_contents($url));
     return [
         'title' => $faker->sentence(3),
         'content' => $faker->Text(3000),
         'user_id' => User::all()->random()->id,
         'user_id' => Category::all()->random()->id,
-        'image' => 'https://picsum.photos/id/'. rand(1,1000) . '/400/300',
+        'image' => $imageName
     ];
 });
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,12 +18,16 @@ use Illuminate\Support\Str;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    $url = 'https://api.adorable.io/avatars/240/'. rand(1,1000);
+    $contents = file_get_contents($url);
+    $imageName = $faker->md5() . '.png';
+    Storage::disk('images')->put( $imageName , file_get_contents($url));
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'rule' => rand(0,3),
-        'image' => 'https://api.adorable.io/avatars/240/'. rand(1,1000),
+        'image' => $imageName,
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
